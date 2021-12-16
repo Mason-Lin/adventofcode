1681a692898eca7a506f35f55d0dd78a8692072e
Mason Lin <mason.lin1@hp.com>
2021-12-16 23:54:23 +0800

    fix

--- day1/main.py
+++ day1/main.py
@@ -3,8 +3,8 @@ from pathlib import Path
 measurements = Path("data").read_text().splitlines()
 
 print(len(measurements))
-# measurements = [1,2,3,2,1,2,1,0,5,8,7,5,6000,5,7]
-increases = 0
+# measurements = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
+increases = 1
 previous = None
 for m in measurements:
     if previous is not None and m > previous:
